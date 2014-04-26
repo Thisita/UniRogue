51d1114c334c8b7bec374fcfcfb6ac6ea271df2c
Ryan <grenderk@live.com>
2014-04-25 20:51:03 -0400

    Initial Scene
    Added Character controller assets and created platform spawning logic

---
+++ Assets/SpawnPlatform.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPlatform : MonoBehaviour {
+
+    public Transform spawnPoint;
+    public Warp warp;
+    public GameObject[] platformPrefabs;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject newPlatform = (GameObject)Instantiate(platformPrefabs[0], spawnPoint.position, Quaternion.identity);
+
+        Warp otherWarp = newPlatform.GetComponentInChildren<Warp>();
+
+        if (otherWarp != null)
+        {
+            GameObject warpPoint = (GameObject)Instantiate(new GameObject("warpPoint"), otherWarp.transform.position, Quaternion.identity);
+            warpPoint.transform.Translate(0, 1, -10);
+            warp.warpTransform = warpPoint.transform;
+        }
+        
+    }
+}
