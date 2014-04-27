116165bbab05dfd43616626d8b39e64ff77efa22
Ryan <grenderk@live.com>
2014-04-27 00:31:52 -0400

    Player Health
    Created a player health script and attached it to the player

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public float playerHealth = 100f;
+    float currentHealth;
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = playerHealth;
+	}
+
+    void TakeDamage(float amt)
+    {
+        currentHealth -= amt;
+
+        if (currentHealth <=0 )
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+
+    }
+	
+}
