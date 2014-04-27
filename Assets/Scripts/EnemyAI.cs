bb0d07a86d57a219106a45a0c1917e3921075d10
Ian Zachary Ledrick <thisita@outlook.com>
2014-04-27 01:23:47 -0400

    Implement tag usage and logging

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class EnemyAI : MonoBehaviour
 {
     // Some configuration
-    public Transform player;
+    GameObject player;
     public float moveSpeed;
     public float minDistance;
     public float maxDistance;
@@ -12,24 +12,40 @@ public class EnemyAI : MonoBehaviour
 	// Use this for initialization
 	void Start()
     {
-	    
+        // Debug
+        if (Debug.isDebugBuild)
+        {
+            Debug.Log("EnemyAI::Start()");
+        }
+        // Get the player
+        player = GameObject.FindGameObjectWithTag("player");
 	}
 	
 	// Update is called once per frame
 	void Update()
     {
         // Track the player via rotation
-        transform.LookAt(player);
+        transform.LookAt(player.transform);
 
         // should we chase?
-        if (Vector3.Distance(transform.position, player.position) >= minDistance)
+        if (Vector3.Distance(transform.position, player.transform.position) >= minDistance)
         {
+            // Debug
+            if (Debug.isDebugBuild)
+            {
+                Debug.Log("EnemyAI::Update() [Chasing]");
+            }
             // chase
             transform.position += transform.forward * moveSpeed * Time.deltaTime;
 
             // Can we attack?
-            if (Vector3.Distance(transform.position, player.position) <= maxDistance)
+            if (Vector3.Distance(transform.position, player.transform.position) <= maxDistance)
             {
+                // Debug
+                if (Debug.isDebugBuild)
+                {
+                    Debug.Log("EnemyAI::Update() [Attacking]");
+                }
                 // attack
                 Attack();
             }
@@ -39,6 +55,11 @@ public class EnemyAI : MonoBehaviour
     // Carries out an attack operation on the player
     void Attack()
     {
+        // Debug
+        if (Debug.isDebugBuild)
+        {
+            Debug.Log("EnemyAI::Attack()");
+        }
         // TODO: Implement
     }
 }
