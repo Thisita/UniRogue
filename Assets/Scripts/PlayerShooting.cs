b9aac5681ef40f3eb8f98485e4c599b3e4f33d8f
Ryan <grenderk@live.com>
2014-04-26 21:53:57 -0400

    Shooting Script
    Created initial Shooting script, health logic not yet implemented

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : MonoBehaviour {
+
+    public float fireRate = 0.5f;
+    float cooldown = 0;
+
+	// Update is called once per frame
+	void Update () {
+
+        cooldown -= Time.deltaTime;
+
+        if(Input.GetButton("Fire1"))
+        {
+            Fire();
+        }
+	}
+
+    void Fire()
+    {
+        if (cooldown > 0)
+        {
+            return;
+        }
+
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        Transform hitTransform;
+        Vector3 hitPoint;
+
+        hitTransform = FindClosestHitObject(ray, out hitPoint);
+
+        if (hitTransform != null)
+        {
+            Debug.Log("Hit object: " + hitTransform.name);
+
+            //Health logic here
+        }
+
+        cooldown = fireRate;
+    }
+
+    Transform FindClosestHitObject(Ray ray, out Vector3 hitPoint)
+    {
+        RaycastHit[] hits = Physics.RaycastAll(ray);
+
+        Transform closestHit = null;
+        float distance = 0;
+        hitPoint = Vector3.zero;
+
+        foreach(RaycastHit hit in hits)
+        {
+            if (hit.transform != this.transform && (closestHit == null || hit.distance < distance))
+            {
+                closestHit = hit.transform;
+                distance = hit.distance;
+                hitPoint = hit.point;
+            }
+        }
+
+        return closestHit;
+    }
+}
