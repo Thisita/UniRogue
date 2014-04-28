b33fda94a6b2dc8f0e24bbc6cd220955a07d4630
Ian Zachary Ledrick <thisita@outlook.com>
2014-04-27 20:07:32 -0400

    Implement Robo animation controller

--- Assets/Scripts/RoboAnimController.cs
+++ Assets/Scripts/RoboAnimController.cs
@@ -3,15 +3,30 @@ using System.Collections;
 
 public class RoboAnimController : MonoBehaviour
 {
+    public enum RoboAnimation
+    {
+        PunchHighLeft = "punch_hi_left",
+        PunchHighRight = "punch_hi_right",
+        KickJumpRight = "kick_jump_right",
+        KickLowRight = "kick_lo_right"
+    }
+    Animation anim;
     // Use this for initialization
 	void Start ()
     {
-	
+        anim = GetComponent<Animation>();
 	}
 	
-	// Update is called once per frame
-	void Update ()
+	// Play a anim based on its name
+    public void PlayAnimation(RoboAnimation ani)
     {
-	
-	}
+        anim.CrossFade(ani.ToString());
+        while (anim.isPlaying)
+        {
+            // do nothing
+            continue;
+        }
+
+        anim.CrossFade("loop_idle");
+    }
 }
